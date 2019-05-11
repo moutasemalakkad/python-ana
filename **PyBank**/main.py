6d29b157c3c4140811cbe4c02ba751d51fb3c20c
Moutasem Akkad <moutasemalakkad@gmail.com>
2019-05-11 14:10:53 -0400

    average problem fixed

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,63 +1,76 @@
 import csv
-
-
 path = '/Users/moutasemakkad/Desktop/HomeWorks/python-challenge/budget_data.csv'
-
 
 with open(path, 'r', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
  
-
     header = next(csvreader)
-
-
-
-    totalMonth = 0
-    totalProfitAndLoss = 0
+    first_row = next(csvreader)
+    totalMonth = 1
+    totalProfitAndLoss = int(first_row[1])
 
     # Average Change
-
     totalChange = 0
     
 
     #The greatest increase in profits (date and amount) over the entire period
-
     greatestIncrease = 0
 
     #   The greatest decrease in losses (date and amount) over the entire period
-
-
-    greatesDecrease = 100000000000
+    greatestDecrease = 100000000000
 
     
     # previous number
+    previousnumber = int(first_row[1])
 
-    previousnumber = 0
+    # Max Month
+    maxMonth = ''
 
+    # Min Month 
+    minMonth = ''
+
+    #list
+    listt = []
 
 
     for row in csvreader:
-        
         totalMonth += 1
         totalProfitAndLoss += int(row[1])
+
         change = int(row[1]) - previousnumber
         totalChange += change
-        
+        listt.append(change)
+        listtLength = len(listt)
+        listtSum = sum(listt)
         
 
         if (int(row[1]) - previousnumber) > greatestIncrease:
             greatestIncrease = (int(row[1]) - previousnumber)
+            maxMonth = row[0]
 
-        if (int(row[1]) - previousnumber) < greatesDecrease:
-            greatesDecrease = (int(row[1]) - previousnumber)
+        if (int(row[1]) - previousnumber) < greatestDecrease:
+            greatestDecrease = (int(row[1]) - previousnumber)
+            minMonth = row[0]
         
         previousnumber = int(row[1])
 
-    print('totalmonth:', ' ', totalMonth)
+
+
+
+
+
+    print("")
+    print('total months:', ' ', totalMonth)
+    print("")
     print('total Profit and Loss: ',totalProfitAndLoss)
-    print('Average', totalChange/totalMonth-1 * 100)
-    print('Greatest Decrease ', greatesDecrease)
-    print('Greates Increase ',greatestIncrease)
+    print("")
+    print('Average Change', totalChange/listtLength * 100)
+    #print('Average Change', listtSum/totalMonth * 100)
+    print("")
+    print('Greatest Decrease ',  f' ({minMonth})', greatestDecrease)
+    print("")
+    print('Greates Increase ', f' ({maxMonth})', greatestIncrease)
+    print("")
         
 
  
